0c46d09b3d5a412c5141f4dba9ae95b40ebc4d55
Yonatan Lavie <yonatan.lavie89@gmail.com>
2021-05-04 18:17:21 +0300

    install npm: bootstrap, popper.js, jquery, lodash, react-icons, moment, react-momet, redux, react-redux

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../css/App.css';
+import Badge from 'react-bootstrap/Badge'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+      <Badge pill variant="info">
+        Hello world!
+      </Badge>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
